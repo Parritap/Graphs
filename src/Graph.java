10d521ebb8370f1375d310beb51ad0c2f5937c67
Juan Esteban Parra Parra <juane.parrap@uqvirtual.edu.co>
2023-08-27 20:36:55 -0500

    Original

--- src/Graph.java
+++ src/Graph.java
@@ -21,7 +21,7 @@ class Graph {
     Graph(int numnodes) {
         num_nodes = numnodes;
         adj = new LinkedList[numnodes];
-        for (int i = 0; i < numnodes; ++i)
+        for (int i = 0; i < numnodes; i++)
             adj[i] = new LinkedList<>();
     }
 
@@ -63,42 +63,6 @@ class Graph {
         pathCount = countPathsUtil(s, d, pathCount);
         return pathCount;
     }
-
-    Integer[][] getPowerTrailMatrix() {
-        Integer[][] matrix = new Integer[7][7];
-        int trails = 0;
-        for (int i = 1; i <= this.num_nodes; i++) {
-            for (int j = 1; j <= this.num_nodes; j++) {
-                trails = countPathsOf_K_trails(i, j, 3);
-                matrix[i - 1][j - 1] = trails;
-            }
-        }
-
-        return new Integer[2][2];
-    }
-
-    private int countPathsOf_K_trails(int s, int d, int k) {
-        return countPaths_k_trails_util(s, d, k, 0, 1);
-    }
-
-    private int countPaths_k_trails_util(int u, int d, int k, int pathCount, int iterations) {
-        if (u == d && iterations==k) {
-            pathCount++;
-            iterations = 0;
-        }
-
-        // Recur for all the vertices
-        // adjacent to this vertex
-        else {
-            Iterator<Integer> i = adj[u].listIterator();
-            while (i.hasNext()) {
-                int n = i.next();
-                pathCount = countPaths_k_trails_util(n, d, pathCount, k,iterations+1);
-            }
-        }
-        return pathCount;
-    }
-
 
     // Driver Code
     public static void main(String args[]) {
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,31 @@
 public class Main {
     public static void main(String[] args) {
         Graph graph1 = new Graph(7);
+        graph1.addEdge(0,1);
+        graph1.addEdge(0,3);
+        graph1.addEdge(0,5);
+
+        graph1.addEdge(1,0);
         graph1.addEdge(1,2);
         graph1.addEdge(1,4);
-        graph1.addEdge(1,6);
 
         graph1.addEdge(2,1);
-        graph1.addEdge(2,3);
-        graph1.addEdge(2,5);
+        graph1.addEdge(2,4);
+        graph1.addEdge(2,6);
 
+        graph1.addEdge(3,0);
+        graph1.addEdge(3,4);
         graph1.addEdge(3,2);
-        graph1.addEdge(3,5);
-        graph1.addEdge(3,7);
 
+        graph1.addEdge(4,0);
         graph1.addEdge(4,1);
-        graph1.addEdge(4,5);
+        graph1.addEdge(4,2);
         graph1.addEdge(4,3);
 
-        graph1.addEdge(5,1);
-        graph1.addEdge(5,2);
-        graph1.addEdge(5,3);
-        graph1.addEdge(5,4);
+        graph1.addEdge(5,0);
+        graph1.addEdge(6,2);
 
-        graph1.addEdge(6,1);
-        graph1.addEdge(7,3);
-
-
+        var x = graph1.countPaths(1,2);
+        System.out.println(x);
     }
 }
